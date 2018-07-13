8922c93d27fa89473bf35021c02421d8d2bf485d
SchetininVitaliy <sch_vitaliy@mail.ru>
2018-07-13 08:25:36 +0300

    airfoil.py example fixes

--- examples/airfoil.py
+++ examples/airfoil.py
@@ -12,27 +12,21 @@ k['tb_l'] = 1.8255
 k['alpha_b'] = 11.6983
 k['alpha_c'] = 3.8270
 
-x = NURBS(k)
-pts = x._spline()
-
-def show(pts):
-	plt.xlim(0,1)
-	plt.ylim(-.2,.2)
-	plt.plot(pts[3],pts[2]) #plot the spline
-	plt.plot(pts[1],pts[0])
-	plt.show()
-
-#show(pts)
+generator = NURBS(k)
+pts = generator._spline()
 
 x = np.concatenate((pts[3],np.flip(pts[1],0)))
 y = np.concatenate((pts[2],np.flip(pts[0],0)))
 
-plt.xlim(0,1)
-plt.ylim(-.2,.2)
-plt.plot(x,y)
-plt.show()
-
 np.savetxt('airfoil.dat',np.c_[x,y])
+
+def show(x,y):
+	plt.xlim(0,1)
+	plt.ylim(-.2,.2)
+	plt.plot(x,y)
+	plt.show()
+
+show(x,y)
 
 res = find_3D_coefficients(airfoil='airfoil.dat',alpha=12.,NACA=False)
 print res
